8ef36b8a0aee337d98e4136f7cd623379f9707bf
lbcamargo94 <lb.camargo94@gmail.com>
2022-06-08 13:16:05 -0300

    feat(./src/components/Header.jsx): Criado novo component Header no arquivo "Header.jsx"

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,1 +1,15 @@
 import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header-solar-system">
+        <h1 className="title-solar-system">
+          Sistema Solar
+        </h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
