cdc39a6fb3bc1034e1c5e08bd9b52f006890193d
lbcamargo94 <lb.camargo94@gmail.com>
2022-06-08 13:19:43 -0300

    feat(./src/App.jsx): Importado "Header" no arquivo "App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './Components/Header';
 
 class App extends React.Component {
   render() {
-    return ;
+    return (
+      <>
+        <Header />
+      </>
+    );
   }
 }
 
