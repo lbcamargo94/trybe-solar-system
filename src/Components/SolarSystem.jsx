70ee9bec41311b6a46433c97e226bbaab00e1052
lbcamargo94 <lb.camargo94@gmail.com>
2022-06-08 14:22:21 -0300

    feat(./src/components): Criado novo arquivo "Title.jsx" na pasta "Components"

--- src/Components/SolarSystem.jsx
+++ src/Components/SolarSystem.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Title from './Title';
 import planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
     return (
       <>
+        <Title headline="Planetas" />
         <div>
           {
             planets.map((el) => {el})
